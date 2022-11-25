b3a26600327a65971b28374041d60268131db755
reyesbrostec <119060714+reyesbrostec@users.noreply.github.com>
2022-11-25 09:37:14 -0500

    Update amadeus.py

--- amadeus.py
+++ amadeus.py
@@ -7,7 +7,7 @@ import openai
 #OpenAI API key
 aienv = os.getenv('OPENAI_KEY')
 if aienv == None:
-    openai.api_key = "Your OpenAI key"
+    openai.api_key = "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"
 else:
     openai.api_key = aienv
 print(aienv)
@@ -15,7 +15,7 @@ print(aienv)
 #Telegram bot key
 tgenv = os.getenv('TELEGRAM_KEY')
 if tgenv == None:
-    tgkey = "Your Telegram Key"
+    tgkey = "5694944439:AAG3QkSiApNQHg-pC3F1v6Q0-DAuONEEy1w"
 else:
     tgkey = tgenv
 print(tgenv)
@@ -35,8 +35,8 @@ running = False
 cache = None
 qcache = None
 chat_log = None
-botname = 'Makise Kurisu'
-username = 'Your name here'
+botname = 'PsiAiris'
+username = 'Usuario'
 # Max chat log length (A token is about 4 letters and max tokens is 2048)
 max = int(3000)
 
